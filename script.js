8c3b1e1070a1d6a40b1ea93f7091459030ec7eff
dudumontz <carlos15montezuma@gmail.com>
2026-08-11 15:07:10 -0300

    Validação caso o número inicial seja maior que o número final.

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".value01").value)
     const max = Math.floor(document.querySelector(".value02").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min
+    if (min >= max) {
+        alert("O primeiro valor precisa ser menor que o segundo!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+    
+        const textResult = document.querySelector(".result")
+    
+        textResult.innerHTML = `O número sorteado é: ${result}`
+    }
 
-    const textResult = document.querySelector(".result")
-
-    textResult.innerHTML = `O número sorteado é: ${result}`
 }
